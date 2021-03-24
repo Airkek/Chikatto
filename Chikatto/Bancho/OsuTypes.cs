65358d9107b4c4cad3b0fd237ed8027cd0dc0384
Keijia <antonyblack05@gmail.com>
2021-03-24 21:29:24 +0300

    ооо типы осу повезло повезло

---
+++ Chikatto/Bancho/OsuTypes.cs
@@ -0,0 +1,34 @@
+﻿namespace Chikatto.Bancho
+{
+    public enum OsuTypes
+    {
+        #region Integer
+        Int8 = 0,
+        UInt8 = 1,
+        Int16 = 2,
+        UInt16 = 3,
+        Int32 = 4,
+        UInt32 = 5,
+        FInt32 = 6,
+        Int64 = 7,
+        UInt64 = 8,
+        FInt64 = 9,
+        #endregion
+
+        #region Osu
+        Message = 11,
+        Channel = 12,
+        Match = 13,
+        ScoreFrame = 14,
+        MapInfoRequest = 15,
+        MapInfoReply = 16,
+        #endregion
+
+        #region Misc
+        Int32List2L = 17,
+        Int32List4L = 18,
+        String = 19,
+        Raw = 20
+        #endregion
+    }
+}
