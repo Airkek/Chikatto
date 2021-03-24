1d34fbd5051205378454dfc1780fe1f1a0cc3b9a
Keijia <antonyblack05@gmail.com>
2021-03-24 23:11:04 +0300

    фыв

--- Chikatto/Bancho/Enums/DataType.cs
+++ Chikatto/Bancho/Enums/DataType.cs
@@ -3,32 +3,30 @@
     public enum DataType
     {
         #region Integer
-        Int8 = 0,
-        UInt8 = 1,
-        Int16 = 2,
-        UInt16 = 3,
-        Int32 = 4,
-        UInt32 = 5,
-        FInt32 = 6,
-        Int64 = 7,
-        UInt64 = 8,
-        FInt64 = 9,
+        Short,
+        UShort,
+        Int,
+        UInt,
+        Float,
+        Long,
+        ULong,
+        Double,
         #endregion
 
         #region Osu
-        Message = 11,
-        Channel = 12,
-        Match = 13,
-        ScoreFrame = 14,
-        MapInfoRequest = 15,
-        MapInfoReply = 16,
+        Message,
+        Channel,
+        Match,
+        ScoreFrame,
+        MapInfoRequest,
+        MapInfoReply,
         #endregion
 
         #region Misc
-        Int32List2L = 17,
-        Int32List4L = 18,
-        String = 19,
-        Raw = 20
+        IntList2L,
+        IntList4L,
+        String,
+        Raw
         #endregion
     }
 }
