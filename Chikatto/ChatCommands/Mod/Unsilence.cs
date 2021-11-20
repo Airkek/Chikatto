4b1c2fff4a5b9cae3e06d3a8fbb90af9c9fa4748
Keijia <antonyblack05@gmail.com>
2021-11-20 21:41:16 +0300

    add unsilence command

---
+++ Chikatto/ChatCommands/Mod/Unsilence.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Chikatto.Enums;
+using Chikatto.Objects;
+using Chikatto.Utils;
+
+namespace Chikatto.ChatCommands.Mod
+{
+    public class Unsilence
+    {
+        [Command(new[] {"unsilence", "unmute"}, "Unmute user", Privileges.Mod)]
+        public static async Task<string> Handle(Presence user, string[] args)
+        {
+            if (args.Length != 1)
+                return $"Usage: {Global.Config.CommandPrefix}unsilence <safename>";
+
+            var username = Auth.GetSafeName(args[0]);
+
+            var toUnmute = await Presence.FromDatabase(username);
+
+            if (toUnmute is null)
+                return $"User <{username}> not found";
+            
+            if (!toUnmute.Silenced)
+                return $"{toUnmute} is not silenced";
+
+            await toUnmute.Mute(-toUnmute.SilenceEndRelative, ""); // :D
+
+            return $"{toUnmute} has unsilenced";
+        }
+    }
+}
